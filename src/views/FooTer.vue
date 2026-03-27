24c56d89d73eda198cf839e7300f9e2fa96eefea
Soidul Sk <soidul@reddensoft.com>
2026-03-27 17:18:29 +0530

    issue fixing

--- src/views/FooTer.vue
+++ src/views/FooTer.vue
@@ -81,7 +81,7 @@
                   </svg>+1(727) 300-6244</a>
                 </li>
                 <li>
-                  <a href="mailto:bigwavedevelopment">
+                  <a href="https://teams.microsoft.com/l/chat/0/0?users=bigwavedevelopment@gmail.com" target="_blank">
                     
 
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-microsoft-teams" viewBox="0 0 16 16">
@@ -89,6 +89,15 @@
                       <path d="M.682 3.349h6.822c.377 0 .682.305.682.682v6.822a.68.68 0 0 1-.682.682H.682A.68.68 0 0 1 0 10.853V4.03c0-.377.305-.682.682-.682Zm5.206 2.596v-.72h-3.59v.72h1.357V9.66h.87V5.945z"/>
                     </svg>
                     bigwavedevelopment</a>
+                </li>
+                <li>
+                  <a href="https://t.me/bigwave_llc" target="_blank">
+                    
+
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telegram" viewBox="0 0 16 16">
+                      <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.287 5.906q-1.168.486-4.666 2.01-.567.225-.595.442c-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294q.39.01.868-.32 3.269-2.206 3.374-2.23c.05-.012.12-.026.166.016s.042.12.037.141c-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8 8 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629q.14.092.27.187c.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.4 1.4 0 0 0-.013-.315.34.34 0 0 0-.114-.217.53.53 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09"/>
+                    </svg>
+                    @bigwave_llc</a>
                 </li>
                 <li>
                   <a href="mailto:david@bigwavedevelopment.com"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
